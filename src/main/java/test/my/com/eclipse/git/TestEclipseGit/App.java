110b21897f60a81abace1695b68de2412cfa9c45
raobinghua <raobinghua@smyfinancial.com>
2018-06-30 20:11:15 +0800

    初次

---
+++ src/main/java/test/my/com/eclipse/git/TestEclipseGit/App.java
@@ -0,0 +1,13 @@
+package test.my.com.eclipse.git.TestEclipseGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
