ab8c011387e7d640d3203d0b9ae967f3a36e22e6
raobinghua <raobinghua@smyfinancial.com>
2018-08-03 18:28:10 +0800

    idea测试

--- src/main/java/test/my/com/eclipse/git/TestEclipseGit/App.java
+++ src/main/java/test/my/com/eclipse/git/TestEclipseGit/App.java
@@ -3,4 +3,8 @@ package test.my.com.eclipse.git.TestEclipseGit;
 
 public class App {
 
+    public static void main(String[] args) {
+        System.out.println("你妹");
+        System.out.println("你姐姐");
+    }
 }
